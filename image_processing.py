ab8f5dbf2ff9f3f271b99bc2fbecc78beb9f869c
Andrew Fulton <andrewfulton9@gmail.com>
2016-07-26 14:38:10 -0600

    made it so processed image files are sent to an s3 bucket instead of kept locally

--- image_processing.py
+++ image_processing.py
@@ -1,7 +1,6 @@
 import boto
 from boto.s3.key import Key
 import skimage
-from skimage.viewer import ImageViewer
 from skimage.transform import resize
 import numpy as np
 import tempfile
@@ -22,21 +21,24 @@ turn list into numpy array
 
 '''
 
-def get_img_array(bucket):
+def get_img_array(in_bucket, out_bucket):
     access_key = os.environ['AWS_ACCESS_KEY_ID1']
     access_secret_key = os.environ['AWS_SECRET_ACCESS_KEY1']
     conn = boto.connect_s3(access_key, access_secret_key)
 
-    b = conn.get_bucket(bucket)
+    b1 = conn.get_bucket(in_bucket)
+    b2 = conn.get_bucket(out_bucket)
+
     files = [f.name for f in b.list()]
 
     ls = []
     temp_dir = tempfile.mkdtemp()
+    temp_dir2 = tempfile.mkdtemp()
 
     for i, f in enumerate(files):
         if i % 1000 == 0:
             print '{}-{}: processing...'.format(i, i+1000)
-        k = b.get_key(f)
+        k = b1.get_key(f)
         fn = temp_dir + '/' + str(f)
         k.get_contents_to_filename(fn)
         try:
@@ -48,18 +50,23 @@ def get_img_array(bucket):
             ls.append(np.transpose(resized))
         os.remove(fn)
         if i % 10000 == 0 and i > 0:
-            arr = np.array(ls)
-            fn = 'data_arrays/' + bucket + \
-                 '_{}'.format(i) + '.npy'
-            np.save(fn, arr)
-            ls = []
+            filename = in_bucket + '_{}'.format(i) + '.npy'
+            fp = temp_dir2 + '/' + filename
+            np.save(fp, np.array(ls))
+            k_out = b2.new_key(filename)
+            k_out.set_contents_from_filename(fp)
+            os.remove(fp)
+
+    filename = in_bucket + '_{}'.format(len(files)) + '.npy'
+    fp = temp_dir2 + '/' + filename
+    np.save(fp, np.array(ls))
+    k = b2.new_key(filename)
+    k.set_contents_from_filename(fp)
+    os.removedirs(temp_dir2)
     os.removedirs(temp_dir)
-    arr = np.arr(ls)
-    fn = '../data_arrays/' + bucket + \
-         '_{}'.format(len(files)) + '.npy'
-    np.save(fn, arr)
     return
 
 if __name__ == '__main__':
-    bucket = sys.argv[1]
-    ls = get_img_array(bucket)
+    input_bucket = sys.argv[1]
+    output_bucket = 'ajfcapstonearrays'
+    ls = get_img_array(input_bucket, output_bucket)
