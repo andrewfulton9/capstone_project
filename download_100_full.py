1437e0da21ee788441f50c152d94e73ce4b232b8
Andrew Fulton <andrewfulton9@gmail.com>
2016-07-28 15:38:55 -0600

    changed file to scrape right thing

--- download_100_full.py
+++ download_100_full.py
@@ -3,4 +3,4 @@ from image_processing import *
 bucket_ls = ['ajfcapstonecars', 'ajfcapstonehome', 'ajfcapstonesavings',
                  'ajfcapstonespecevents', 'ajfcapstonetravel']
 
-process_imgs(bucket_ls, img_size = 100)
+process_imgs(bucket_ls)
