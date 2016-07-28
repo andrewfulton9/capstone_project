359df142ae2dab61937691be829bbe00b69870c2
Andrew Fulton <andrewfulton9@gmail.com>
2016-07-28 15:15:02 -0600

    added new files to download and transform different ways all at once and edited image_processing file for more testing

---
+++ download_half_100.py
@@ -0,0 +1,6 @@
+from image_processing import *
+
+bucket_ls = ['ajfcapstonecars', 'ajfcapstonehome', 'ajfcapstonesavings',
+                 'ajfcapstonespecevents', 'ajfcapstonetravel']
+
+process_imgs(bucket_ls, img_size = 100, sample_size='half')
---
+++ download_half_50.py
@@ -0,0 +1,6 @@
+from image_processing import *
+
+bucket_ls = ['ajfcapstonecars', 'ajfcapstonehome', 'ajfcapstonesavings',
+                 'ajfcapstonespecevents', 'ajfcapstonetravel']
+
+process_imgs(bucket_ls, img_size = 50, sample_size='half')
