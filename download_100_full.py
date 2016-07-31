e76939c93c2f96c6bb5b4892ed9e60cbf965a3ac
Andrew Fulton <andrewfulton9@gmail.com>
2016-07-30 21:59:17 -0600

    made bin sizes smaller

--- download_100_full.py
+++ download_100_full.py
@@ -3,4 +3,4 @@ from image_processing import *
 bucket_ls = ['ajfcapstonecars', 'ajfcapstonehome', 'ajfcapstonesavings',
                  'ajfcapstonespecevents', 'ajfcapstonetravel']
 
-bin_save_arrs(bucket_ls, bin_size = 100000)
+bin_save_arrs(bucket_ls, bin_size = 25000)
--- download_half_100.py
+++ download_half_100.py
@@ -3,4 +3,4 @@ from image_processing import *
 bucket_ls = ['ajfcapstonecars', 'ajfcapstonehome', 'ajfcapstonesavings',
                  'ajfcapstonespecevents', 'ajfcapstonetravel']
 
-bin_save_arrs(bucket_ls, img_size = 100, bin_size=100000, sample_size='half')
+bin_save_arrs(bucket_ls, img_size = 100, bin_size=25000, sample_size='half')
