f18105c2e0998aaddd3f86f8e48f5c84c34af83e
Andrew Fulton <andrewfulton9@gmail.com>
2016-08-01 10:41:14 -0600

    added code so the model weights are saved to an s3 bucket

--- fit_models.py
+++ fit_models.py
@@ -1,16 +1,36 @@
 import CNN
 import image_processing as ip
 import score_models as sm
+import aws_funcs as af
 
-model = CNN.vgg_16(img_size=50)
 
-X_test, y_test = CNN.fit_model_batches('arr_X_50_397326', model = model,
-                                    weights_filename='50_full_vgg16_batchfit')
+def save_weights(bucket = 'ajfcapstoneweights'):
+    b = af.connect_2_s3_bucket(bucket)
+    for f in os.listdir('weights'):
+        path = 'weights/' + f
+        k = b.new_key(f)
+        k.set_contents_from_filename(path)
 
-probs, cats = CNN.predict_model(X_test, model)
+if __name__ == '__main__':
+    model = CNN.vgg_16(img_size=100)
 
-np.save('50_full_vgg16_probs.npy', probs)
-np.save('50_full_vgg16_probs.npy', cats)
+    X_test, y_test = CNN.fit_model_batches('arr_X_100_397326', model = model,
+                                    weights_filename='100_full_vgg16_batchfit')
 
-np.save('X_test.npy', X_test)
-np.save('y_test.npy', y_test)
+    save_weights()
+
+    probs, cats = CNN.predict_model(X_test, model)
+
+    np.save('50_full_vgg16_probs.npy', probs)
+    np.save('50_full_vgg16_cats.npy', cats)
+
+    b = af.connect_2_s3('ajfcapstonearrays')
+
+    np.save('X_test.npy', X_test)
+    np.save('y_test.npy', y_test)
+
+    b.new_key('100_full_vgg16_X_test.npy')
+    b.set_contents_from_filename('X_test.npy')
+
+    b.new_key('100_full_vgg16_y_test.npy')
+    b.set_contents_from_filename('y_test.npy')
