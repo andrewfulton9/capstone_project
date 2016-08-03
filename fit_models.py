1e00e0e96779fb26fe8b39f138c2c53004ba0ab4
Andrew Fulton <andrewfulton9@gmail.com>
2016-08-02 21:28:18 -0600

    edit

--- fit_models.py
+++ fit_models.py
@@ -13,25 +13,25 @@ def save_weights(bucket = 'ajfcapstoneweights'):
         k.set_contents_from_filename(path)
 
 if __name__ == '__main__':
-    model = CNN.vgg_16(img_size=50)
+    model = CNN.vgg_16(img_size=100)
 
-    X_test, y_test = CNN.fit_model_batches('arr_X_50_full', model = model,
-                                    weights_filename='50_full_vgg16_batchfit')
+    X_test, y_test = CNN.fit_model_batches('arr_X_100_full', model = model,
+                                    weights_filename='100_full_vgg16_batchfit')
 
     save_weights()
 
     probs, cats = CNN.predict_model(X_test, model)
 
-    np.save('50_full_vgg16_probs.npy', probs)
-    np.save('50_full_vgg16_cats.npy', cats)
+    np.save('100_full_vgg16_probs.npy', probs)
+    np.save('100_full_vgg16_cats.npy', cats)
 
     b = af.connect_2_s3('ajfcapstonearrays')
 
     np.save('X_test.npy', X_test)
     np.save('y_test.npy', y_test)
 
-    b.new_key('50_full_vgg16_X_test.npy')
+    b.new_key('100_full_vgg16_X_test.npy')
     b.set_contents_from_filename('X_test.npy')
 
-    b.new_key('5 0_full_vgg16_y_test.npy')
+    b.new_key('100_full_vgg16_y_test.npy')
     b.set_contents_from_filename('y_test.npy')
