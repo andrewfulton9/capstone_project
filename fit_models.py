de47d6ce6dd16d806130d849b87e850662ede95b
Andrew Fulton <andrewfulton9@gmail.com>
2016-08-24 16:29:55 -0600

    edits

--- fit_models.py
+++ fit_models.py
@@ -137,7 +137,7 @@ if __name__ == '__main__':
     # build model to fit
     model = CNN.vgg_basic
 
-    cnn = EmployModel(model, 'arr_X_50_full', lr = .001, batch_size=250,
+    cnn = EmployModel(model, 'arr_X_50_full', lr = .0005, batch_size=500,
                       weights_filename = '50_full_basic_batchfit_3')
 
     print 'accuracy: ', cnn.accuracy()
